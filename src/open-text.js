07663b874b83648140859ca767aa54745dbc5567
gigorko1993 <gigorko1993@gmail.com>
2021-02-21 11:11:38 +0200

    зробив відкриття тексту при нажиманні на кнопку в герої

--- src/open-text.js
+++ src/open-text.js
@@ -3,10 +3,7 @@
   const textRef = document.querySelector("[data-text]");
     
   showTextBtnRef.addEventListener("click", () => {
-    const expanded =
-    showTextBtnRef.getAttribute("aria-expanded") === "true" || false;
     showTextBtnRef.classList.toggle("is-shown");
-    showTextBtnRef.setAttribute("aria-expanded", !expanded);
 
     textRef.classList.toggle("is-shown");
   })
