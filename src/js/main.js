443122cde1f5e931928df1e1db28fa99a18c8d4d
Knirta K <myknirtak@gmail.com>
2021-02-22 19:12:19 +0200

    add styles for slider section

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.slider').slick({
     dots: true,
     arrows: false,
-    autoplay: true,
+    autoplay: false,
     infinite: true,
     speed: 250,
     slidesToShow: 1,
