1a6225d005124f3dfa7a09d98dcd9bfc217f5008
Stanislav Sheludyakov <serophiom@gmail.com>
2021-02-23 23:04:04 +0200

    Merge branch 'dev' into gallery-appear

---
+++ src/js/scroll.js
@@ -0,0 +1,26 @@
+ $(document).ready(function(){
+    $("#scroll").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+});
+
+ $(document).ready(function(){
+    $("#scroll-mobile-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+ });
+
+  $(document).ready(function(){
+    $("#scroll-header-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+});
