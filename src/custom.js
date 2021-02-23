8c1ca0dde648342c43e422f2892fb447e095093d
gigorko1993 <gigorko1993@gmail.com>
2021-02-23 17:32:14 +0200

    видалив всі коментарі

--- src/custom.js
+++ src/custom.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     var show = true;
     var countbox = ".benefits__inner";
     $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height = $(window).height(); // Высота окна браузера
-        var d_height = $(document).height(); // Высота всего документа
-        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (!show) return false; 
+        var w_top = $(window).scrollTop(); 
+        var e_top = $(countbox).offset().top; 
+        var w_height = $(window).height(); 
+        var d_height = $(document).height(); 
+        var e_height = $(countbox).outerHeight(); 
         if (w_top + w_height - 50 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $('.benefits__number').css('opacity', '1');
             $('.benefits__number-udder').spincrement({
