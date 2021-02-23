3757db9345c1fb00ffe55ef3ecd3f86533974863
andriy-lanyk <LAM1988@ukr.net>
2021-02-23 12:29:29 +0200

    Добавив скрол для хедера і мобільного меню

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,6 +3,24 @@
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+});
+
+ $(document).ready(function(){
+    $("#scroll-mobile-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+ });
+
+  $(document).ready(function(){
+    $("#scroll-header-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
     });
 });
